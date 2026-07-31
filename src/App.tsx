b1be2430841dc9158b2e96cce1214c35b6140b1a
rubengitdev <rubencahyadi504@gmail.com>
2026-08-01 02:58:07 +0700

    Refactor: Add FootballPlayerCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,17 @@
+import './App.css';
 
+const PlayerCard = () => {
+    return (
+        <div className="card-wrapper tier-gold">
+            <div className="card">
+                <div className="card-header"></div>
+            </div>
+        </div>
+    );
+};
+
+const FootballPlayerCard = () => {
+    return <PlayerCard />;
+};
+
+export default FootballPlayerCard;
