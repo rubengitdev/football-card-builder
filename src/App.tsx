1abaed5e224d14cc07b428ab6e8692ec9b7e2700
rubengitdev <rubencahyadi504@gmail.com>
2026-08-02 17:57:50 +0700

    Refactor: Replace inline object in the FootballPlayerCard component and replace it with global default generic type object of player

--- src/App.tsx
+++ src/App.tsx
@@ -98,6 +98,21 @@ const PlayerCard = ({ player }: { player: PlayerData }) => {
     );
 };
 
+const defaultPlayer: PlayerData = {
+    name: 'PELE',
+    overallRating: 98,
+    position: 'ST',
+    club: 'Santos FC',
+    imageUrl:
+        'https://cdn.freecodecamp.org/curriculum/typescript/tsx-workshop/pele.jpg',
+    pac: 97,
+    sho: 98,
+    pas: 83,
+    dri: 99,
+    def: 41,
+    phy: 75,
+};
+
 const FootballPlayerCard = () => {
     const peleData: PlayerData = {
         name: 'PELE',
